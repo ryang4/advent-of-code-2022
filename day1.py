970e2f0d5b182b9a12c98882842916eb59d252ba
Ryan Galliher <galliher.ryan@gmail.com>
2022-12-01 22:47:10 -0500

    day one

---
+++ day1.py
@@ -0,0 +1,44 @@
+def openFile(filename):
+    
+    f = open(filename, 'r')
+    result = f.readlines()
+    f.close()
+
+    return result
+
+def singleSum(input):
+    currentElfSum = 0
+    maxElfSum = 0
+    for line in input:
+        if len(line.strip()) != 0:
+            currentElfSum += int(line.strip())
+        else:
+            maxElfSum = max(currentElfSum, maxElfSum)
+            currentElfSum = 0
+    
+    return maxElfSum
+
+def xSum(input, x):
+    currentElfSum = 0
+    elfSums = []
+    for line in input:
+        if len(line.strip()) != 0:
+            currentElfSum += int(line.strip())
+        else:
+            elfSums.append(currentElfSum)
+            currentElfSum = 0
+    
+    elfSums = sorted(elfSums)
+    
+    return elfSums[-3:]
+
+
+def main():
+    input = openFile('input.txt')
+    maxElfSum = xSum(input, 3)
+
+    print(sum(maxElfSum))
+
+main()
+    
+
